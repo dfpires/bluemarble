a8c5a791e2ad50b485ce660b566b1a9c7d592890
dfpires@gmail.com <dfpires@gmail.com>
2022-02-22 19:32:30 -0300

    17-02

---
+++ main.cpp
@@ -0,0 +1,15 @@
+
+#include <iostream>
+#include <glm/glm.hpp>
+
+#include <glm/gtx/string_cast.hpp>
+
+int main() {
+
+	glm::vec3 V(1, 2, 3);
+
+	// definindo a saída padrão
+	std::cout << "Boa noite, já sabemos programar em C++" << std::endl;
+	std::cout << glm::to_string(V) << std::endl;
+	return 0;
+}
